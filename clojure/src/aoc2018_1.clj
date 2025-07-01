90307b0d9b994df5ebfe8e36b4eb6b45abc94b7a
david.k <david.k@protopie.io>
2025-07-01 13:48:03 +0900

    chore: add common function ( read-file-to-list, parse-strings-to-numbers )

--- clojure/src/aoc2018_1.clj
+++ clojure/src/aoc2018_1.clj
@@ -1,5 +1,19 @@
 (ns aoc2018-1)
 
+(require '[clojure.java.io :as io])
+;; 공통함수 선언
+
+(defn read-file-to-list
+      "파일 경로를 참조해서 읽고, 리스트를 반환"
+      [file-path]
+      (with-open [rdr (io/reader file-path)]
+                  (doall (line-seq rdr))))
+
+(defn parse-strings-to-numbers
+      "문자열 리스트를 받아서 숫자 리스트로 변환"
+      [string-list]
+      (map #(Integer/parseInt %)
+           string-list))
 ;; 파트 1
 ;; 주어진 입력의 모든 숫자를 더하시오.
 ;; 예) +10 -2 -5 +1 이 입력일 경우 4를 출력
