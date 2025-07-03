6d738862b2125fe3f739b39336a49c49c71e7ed2
david.k <david.k@protopie.io>
2025-07-03 10:24:33 +0900

    chore: 코드리뷰 피드백 반영, part2 문제 풀이

--- clojure/src/aoc2018_3.clj
+++ clojure/src/aoc2018_3.clj
@@ -24,47 +24,85 @@
 
 (def sample-input (clojure.string/split-lines (slurp "resources/aoc2018_3.sample.txt")))
 
-(defn transform-to-map
-      "파싱한 백터를 map 형태로 변환해서 반환한다"
-      [data-vector]
-      {:id (parse-long (nth data-vector 1))
-       :x (parse-long (nth data-vector 2))
-       :y (parse-long (nth data-vector 3))
-       :col-length (parse-long (nth data-vector 4))
-       :row-length (parse-long (nth data-vector 5))})
+(defn transform-data-vector-to-vo-map
+  "
+  Arguments:
+    [*data-vector(vector string)]
+  Returns:
+    map{:id (long), :x (long), :y (long), :col-length (long), :row-length (long)}
+  Description:
+    ex) #1 @ 1,3: 4x4
+    예시에 있는 문자열 같은 규격의 문자에서 정규식으로 추출한 문자열 백터를 입력받는다.
+    입력받은 벡터를 가독성을 높이기 위한 map 타입의 Value Object로 변형하는 함수이다.
+  "
+  [data-vector]
+  {:id (parse-long (nth data-vector 1))
+   :x (parse-long (nth data-vector 2))
+   :y (parse-long (nth data-vector 3))
+   :col-length (parse-long (nth data-vector 4))
+   :row-length (parse-long (nth data-vector 5))})
 
-(defn generate-sub-board
-  "주어진 좌표와 길이로 격자 공간을 채우는 함수"
-  [transformed-row]
-  (for [i (range (:x transformed-row) (+ (:x transformed-row) (:col-length transformed-row)))
-        j (range (:y transformed-row) (+ (:y transformed-row) (:row-length transformed-row)))]
-    {[i j] [(:id transformed-row)]}))
+(defn generate-ids-include-coordinate-map
+  "
+  Arguments:
+    [*vo-map(map(:id (long), :x (long), :y (long), :col-length (long), :row-length (long)))]
+  Returns:
+    map{
+      coordinate (vector long long) ids (vector long),
+      coordinate (vector long long) ids (vector long),
+      ...
+    }
+  Description:
+    ex) map(:id 3, :x 5, :y 5, :col-length 2, :row-length 2)
+    예시에 있는 vo 역할을 하는 map 타입의 입력을 받는다.
+    입력받은 맵의 데이터를 이용해서 격자 공간의 좌표와 id값을 map{coordinate (vector long long) ids (vector long), ...} 형태로 가공한다.
+    반환되는 map에서 value가 vector인 이유는 추후 병합해서 id가 축적되도록 하기 위함이다.
+  "
+  [vo-map]
+  (for [i (range (:x vo-map) (+ (:x vo-map) (:col-length vo-map)))
+        j (range (:y vo-map) (+ (:y vo-map) (:row-length vo-map)))]
+    {[i j] [(:id vo-map)]}))
 
-(defn merge-boards
-  "보드위의 공간을 하나로 병합한다"
-  [liner-board-elements]
+(defn merge-ids-by-value-with-coordinate-map
+  "
+  Arguments:
+    [*vo-map-list(list map(:id (long), :x (long), :y (long), :col-length (long), :row-length (long)))]
+  Returns:
+    vector(coordinate (vector [long long]) ids (list long))
+  Description:
+    ex) map(:id 3, :x 5, :y 5, :col-length 2, :row-length 2), ...
+    예시에 있는 vo 역할을 하는 map list 타입의 입력을 받는다.
+    입력받은 데이터에서 좌표(coordinate)가 중복이라면, 병합된 ids를 갖는 맵을 반환한다.
+  "
+  [vo-map-list]
   (reduce (fn [source target]
             (merge-with
               (fn [old new] (distinct (concat old new)))
               source
               target))
           {}
-          (mapcat generate-sub-board liner-board-elements)))
+          (mapcat generate-ids-include-coordinate-map vo-map-list)))
 
-(def transformed-map
-  (map transform-to-map
-       (map #(re-matches #"#(\d)\s@\s(\d),(\d):\s(\d)x(\d)" %) sample-input)))
+(def transformed-vo-map-list
+  (map transform-data-vector-to-vo-map
+       (map #(re-matches #"#(\d+)\s@\s(\d+),(\d+):\s(\d+)x(\d+)" %) sample-input)))
 
-(println transformed-map)
+(def board (merge-ids-by-value-with-coordinate-map transformed-vo-map-list))
 
-(def merged-board (merge-boards transformed-map))
-
-(count (filter (fn [[key value]]
-                 (< 1 (count value)))
-               merged-board))
-
+(comment
+  (count (filter (fn [[coordinate ids]]
+                   (< 1 (count ids)))
+                 board)))
 
 ;; 파트 2
 ;; 입력대로 모든 격자를 채우고 나면, 정확히 한 ID에 해당하는 영역이 다른 어떤 영역과도 겹치지 않음
 ;; 위의 예시에서는 ID 3 이 ID 1, 2와 겹치지 않음. 3을 출력.
 ;; 겹치지 않는 영역을 가진 ID를 출력하시오. (문제에서 답이 하나만 나옴을 보장함)
+(comment
+  (clojure.set/difference
+    (set (map #(:id %) transformed-vo-map-list))
+    (set (flatten
+           (map #(second %)
+                (filter (fn [[coordinate ids]]
+                          (< 1 (count ids)))
+                        board))))))
