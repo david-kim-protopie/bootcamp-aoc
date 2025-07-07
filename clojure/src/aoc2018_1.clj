d15baafa3eb7205f71febf2daad3d0c3ad32640f
david.k <david.k@protopie.io>
2025-07-07 13:46:07 +0900

    refactor: 함수이름 변경, thread macro 방식으로 호출 변경, 합계 누적하는 reductions 사용

--- clojure/src/aoc2018_1.clj
+++ clojure/src/aoc2018_1.clj
@@ -1,43 +1,58 @@
-(ns aoc2018-1)
-;; docstring에 * prefix -> 필수
+(ns aoc2018-1
+  (:require [clojure.string :as str]))
+;; Helper
+(defn- println-data-bypass
+  "출력하고 데이터 반환하는 헬퍼 함수"
+  [data]
+  (do
+    (println data)
+    data))
+
+;; Parsing
+(defn parse-input-to-lines
+  "파일을 읽어 문자열을 반환합니다."
+  [file-path]
+  (->> file-path
+       slurp
+       str/split-lines))
 
 ;; 파트 1
 ;; 주어진 입력의 모든 숫자를 더하시오.
 ;; 예) +10 -2 -5 +1 이 입력일 경우 4를 출력
 ;; sum
+(defn chronal-calibration-part1
+  "aoc day1 part1 main 함수"
+  [file-path]
+  (->> file-path
+       parse-input-to-lines
+       ;println-data-bypass
+       (map parse-long)
+       (reduce +)))
+
 (comment
-  (reduce +
-          (map parse-long
-            (clojure.string/split-lines (slurp "resources/aoc2018_1.sample.txt")))))
+  (chronal-calibration-part1 "resources/aoc2018_1.sample.txt"))
 
 ;; 파트 2
 ;; 주어진 입력의 숫자를 더할 때 마다 나오는 숫자 중, 처음으로 두번 나오는 숫자를 리턴하시오.
 ;; 예) +3, +3, +4, -2, -4 는 10이 처음으로 두번 나오는 숫자임.
 ;; 0 -> 3 (+3) -> 6 (+3) -> 10(+4) -> 8(-2) -> 4(-4) -> 7(+3) -> 10(+3) -> ...]
 (defn find-twice-appear-number
-  "
-  Arguments:
-    [*[sum(long) duplicate-check-set(set long)]
-      *number(long)]
-  Returns:
-    found -> twice-appear-number(long)
-    not found -> [sum(long) duplicate-check-set(set long)]
-  Description:
-    주어진 숫자들을 더할 때, 처음으로 두번 나오는 숫자를 찾는다.
-    sum은 현재까지의 합계, duplicate-check-set은 중복된 값을 확인하기 위한 집합(set)이다.
-    찾는다면 두 번 등장한 수 반환
-    못찾는다면 reduce를 진행하기 위한 [sum, duplicate-check-set] 반환
-  "
-  [[sum duplicate-check-set]
-   number]
-  (let [new-sum (+ sum number)]
-    ;; contains set, map에는 정상동작하나,
-    (if (duplicate-check-set new-sum)
-      (reduced new-sum)
-      [new-sum (conj duplicate-check-set new-sum)])))
+  "두번째 등장하는 숫자 반환"
+  [seen-set item]
+  (if (seen-set item)
+    (reduced item)
+    (conj seen-set item)))
+
+(defn chronal-calibration-part2
+  "aoc day1 part2 main 함수"
+  [file-path]
+  (->> file-path
+       parse-input-to-lines
+       ;println-data-bypass
+       (map parse-long)
+       (cycle)
+       (reductions +)
+       (reduce find-twice-appear-number #{})))
 
 (comment
-  (reduce find-twice-appear-number
-          [0 #{}]
-          (cycle (map parse-long
-                      (clojure.string/split-lines (slurp "resources/aoc2018_1.sample.part2.txt"))))))
+  (chronal-calibration-part2 "resources/aoc2018_1.sample.txt"))
