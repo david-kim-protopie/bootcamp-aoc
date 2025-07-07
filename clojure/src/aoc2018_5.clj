bd5a5cb0f70d8525e2e7b69c6cbc68ae177fbef6
david.k <david.k@protopie.io>
2025-07-07 17:17:29 +0900

    doc: 코드리뷰 피드백 반영

--- clojure/src/aoc2018_5.clj
+++ clojure/src/aoc2018_5.clj
@@ -1,8 +1,10 @@
 (ns aoc2018_5
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]))                      ;; 매크로가 아닌 경우 추천하지 않음 str/aaa -> 까진 wrap 하지 않음
 
+
+;; 디버깅용 함수는 pr 시 제거
 ;; Helper
-(defn- println-data-bypass
+(defn- println-data-bypass                                  ;; util library
   "출력하고 데이터 반환하는 헬퍼 함수"
   [data]
   (do
@@ -10,6 +12,8 @@
     data))
 
 ;; Parsing
+;; 한 번의 호출로 되면 thread macro 사용 지양
+;; Parsing은 실제 parsing 작업이 있어야함
 (defn parse-input-polymers
   "파일을 읽어 문자열을 반환합니다.
   input : file-path
@@ -33,49 +37,57 @@
   output: boolean"
   [unit1 unit2]
   (and (some? unit1) (some? unit2)
-       (== (get-uppercase-char-ascii unit1) (get-uppercase-char-ascii unit2)) ; 타입이 같고
-       (not= (int unit1) (int unit2))))
+       ;; 대소문자 비교 ascii 값 차이 (abs 32)
+       (== (get-uppercase-char-ascii unit1) (get-uppercase-char-ascii unit2)) ; 타입이 같고 (=, (== 차이점 확인하기
+       (not= (int unit1) (int unit2))))                     ;; cC Cc, cc CC
 
 (defn- react-unit-reducer
   "reduce 연산에 사용될 함수. 누산기와 현재 아이템을 받아 다음 반환.
-  반응이 일어나면 누산기(acc)에서 제거하고, 반응이 일어나지 않으면 현재 문자열을 누산기에 추가한다."
-  [reacted-polymer current-unit]
-  (let [last-unit (peek reacted-polymer)]
+  반응이 일어나면 누산기(stack)에서 제거하고, 반응이 일어나지 않으면 현재 문자열을 누산기에 추가한다.
+  input: 누산기(stack), 현재 문자
+  output: 반응이 끝난 누산기
+  "
+  [polymer-stack current-unit]
+  (let [last-unit (peek polymer-stack)]
     (if (reaction? last-unit current-unit)
-      (pop reacted-polymer)
-      (conj reacted-polymer current-unit))))
+      (pop polymer-stack)
+      (conj polymer-stack current-unit))))
 
-(defn polymers->distinct-unit-set
-  "폴리머에서 중복제거를 한 소문자 set을 반환합니다."
+(defn polymers-to-distinct-unit-set
+  "폴리머에서 중복제거를 한 소문자 set을 반환합니다.
+  input : abAbAa
+  output #{a b}"
   [polymers]
   (->> polymers
        (str/lower-case)
-       (seq)
-       (map #(str %))
+       (seq)                                                ;; string이 이미 seq
+       (map str)                                            ;; #() 사용하는 케이스 - 넣는 인자값이 2개 이상일 때 사용, 아니면 그냥 변수만
        (set)))
 
-(defn- remove-character-polymers
-  "문자열에서 특정 문자를 제거한 문자열을 반환합니다."
+;; number n, string s, character c, ...
+(defn- remove-character-polymers-case-insensitive                ;; case insensitive, 유틸성 함수는 범용적으로 사용하기
+  "문자열에서 특정 문자를 대소문자 구분없이 제거한 문자열을 반환합니다."
   [polymers character]
   (let [pattern (re-pattern (str "(?i)" character))]
     (str/replace polymers pattern "")))
 
 ;; Aggregate
 (defn fully-react-polymers
-  "폴리머 문자열의 모든 연쇄 반응을 완료시킨 최종 폴리머를 반환합니다."
+  "폴리머 문자열의 모든 반응을 완료시킨 최종 폴리머를 반환합니다."
   [polymers]
   (->> polymers
        (reduce react-unit-reducer [])
        (apply str)))
 
 ;; Print
+;; 단일 호출같은 경우는 함수 선언하지 말기
 (defn count-polymers-length
   "최종 폴리머의 길이를 계산하여 반환합니다."
   [final-polymers]
   (count final-polymers))
 
 ;; 파트 1
-;; 입력: dabAcCaCBAcCcaDA
+;; 입력: dabAaCBAcCcaDA
 
 ;; 같은 종류의 소문자와 대문자는 서로 ‘반응‘하여 사라짐. aABb -> ‘’
 ;; 사라진 자리는 진공이 되기 때문에 다른 문자들이 붙게 되고, 또 그 문자들끼리 반응할 수 있음.  abBA-> aA -> ‘’
@@ -84,6 +96,9 @@
 ;; 예시 dabAcCaCBAcCcaDA => dabCBAcaDA
 
 ;; 주어진 input 에서 최종으로 남는 문자열을 리턴하시오.
+;; reduce 맵에 상태 갱신하는 경우 사용하면 좋음
+;; [x y & more ] => 가변인자 함수 apply 쓰는게 자연스러움
+;; (+ 1 2)
 
 (defn alchemical-reduction-part1
   "aop 2018 day5 part1 main 함수"
@@ -91,7 +106,7 @@
   (->> file-path
        (parse-input-polymers)
        (fully-react-polymers)
-       (println-data-bypass)
+       #_(println-data-bypass)                              ;; 평가 가능, 실행결과 영향 x, #_ 중첩가능
        (count-polymers-length)))
 
 (comment
@@ -106,9 +121,9 @@
   "aop 2018 day5 part2 main 함수"
   [file-path]
   (let [polymers (parse-input-polymers file-path)
-        distinct-units (polymers->distinct-unit-set polymers)]
+        distinct-units (polymers-to-distinct-unit-set polymers)]
     (->> distinct-units
-         (map #(remove-character-polymers polymers %))
+         (map #(remove-character-polymers-case-insensitive polymers %))
          (map fully-react-polymers)
          (map count-polymers-length)
          (apply min))))
