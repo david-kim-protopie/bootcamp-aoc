9b85cf502c228038d72cb58004636327f6866665
david.k <david.k@protopie.io>
2025-07-16 15:46:31 +0900

    refactor: day 2 리펙토링

--- clojure/src/aoc2018_2.clj
+++ clojure/src/aoc2018_2.clj
@@ -1,34 +1,17 @@
 (ns aoc2018-2
   (:require [clojure.string :as str]))
-
 ;; Helper
 (defn- println-data-bypass
   "출력하고 데이터 반환하는 헬퍼 함수"
-  [data]
-  (do
-    (println data)
-    data))
+  ([data]
+   (do
+     (println data)
+     data))
+  ([description data]
+   (do
+     (println description data)
+     data)))
 
-;; Parsing
-(defn read-file-lines
-  "파일을 읽어 각 줄을 문자열로 하는 리스트(시퀀스)를 반환합니다."
-  [file-path]
-  (->> file-path
-       (slurp)
-       (str/split-lines)))
-
-(defn- character-frequency
-  "문자열에서 문자의 등장빈도를 반환합니다."
-  [characters]
-  (frequencies characters))
-
-(defn part1
-  [file-path]
-  (->> file-path
-       read-file-lines
-       map seq))
-
-(part1 "resources/aoc2018_2.sample.part2.txt")
 ;; 파트 1
 ;; 주어진 각각의 문자열에서, 같은 문자가 두번 혹은 세번씩 나타난다면 각각을 한번씩 센다.
 ;; 두번 나타난 문자가 있는 문자열의 수 * 세번 나타난 문자가 있는 문자열의 수를 반환하시오.
@@ -41,17 +24,42 @@
 ;; abcdee 2개의 e -> (두번 나오는 문자열 수: 4, 세번 나오는 문자열 수: 2)
 ;; ababab 3개의 a, 3개의 b 지만 한 문자열에서 같은 갯수는 한번만 카운트함 -> (두번 나오는 문자열 수: 4, 세번 나오는 문자열 수: 3)
 ;; 답 : 4 * 3 = 12
+
+(def sample-input-part1 (str/split-lines "abcdef
+bababc
+abbcde
+abcccd
+aabcdd
+abcdee
+ababab"))
+
+;; Parsing
+(defn parse-freq-set
+  "문자열에서 문자가 중복등장하는 수를 set으로 중복제거하도록 변환해서 반환한다.
+  input: {a 1, b 2, c 2}
+  output: #{1, 2, 3}"
+  [frequencies-map]
+  (let [distinct-freq_fn (fn [freq-map]
+                           (->> (vals freq-map)
+                                (set)))]
+    (distinct-freq_fn frequencies-map)))
+
+;; Aggregate
+(defn solve-part1
+  [input]
+  (let [frequencies-set (->> (map frequencies input)
+                             (map parse-freq-set))]
+    (* (count (filter #(% 2) frequencies-set))
+       (count (filter #(% 3) frequencies-set)))))
+
+(defn inventory-management-system-part1
+  [input]
+  (solve-part1 input))
+
 (comment
-  (def judge-completed-list
-    (map (fn [freq-map]
-           (assoc freq-map
-             :2-exists (some #(= 2 (val %)) freq-map)
-             :3-exists (some #(= 3 (val %)) freq-map)))
-         (map frequencies (clojure.string/split-lines (slurp "resources/aoc2018_2.sample.txt")))))
-
-  (* (count (filter :2-exists judge-completed-list))
-     (count (filter :3-exists judge-completed-list))))
-
+  #_(inventory-management-system-part1 sample-input-part1)
+  (inventory-management-system-part1 (->> (slurp "resources/aoc2018_2.sample.txt")
+                                          (str/split-lines))))
 ;; 파트 2
 ;; 여러개의 문자열 중, 같은 위치에 정확히 하나의 문자가 다른 문자열 쌍에서 같은 부분만을 리턴하시오.
 ;; 예)
@@ -62,28 +70,7 @@
 ;; fguij
 ;; axcye
 ;; wvxyz
-(defn similarity-scoring-string-pair
-  "
-  Arguments:
-    [*source-string(string) target-string(string)]
-  Returns:
-    found -> twice-appear-number(long)
-    not found -> [sum(long) duplicate-check-set(set long)]
-  Description:
-    주어진 숫자들을 더할 때, 처음으로 두번 나오는 숫자를 찾는다.
-    sum은 현재까지의 합계, duplicate-check-set은 중복된 값을 확인하기 위한 집합(set)이다.
-    찾는다면 두 번 등장한 수 반환
-    못찾는다면 reduce를 진행하기 위한 [sum, duplicate-check-set] 반환
-  "
-  [source-string target-string]
-  (let [source-letters (seq source-string)
-        target-letters (seq target-string)]
-    (reduce (fn [score [source target]]
-              (if (= source target)
-                score
-                (inc score)))
-            0
-            (map vector source-letters target-letters))))
+
 
 ;; else 지양, list를 변형 시킨다.
 ;; transformation for 지양, 고차함수 위주로 작성
@@ -96,30 +83,59 @@
 ;; input / output arguments data type, require 유무 등 docstring에 적어놓기
 ;; js -> functional 치환하는 과정
 ;; ai 적극 활용하기
-(defn find-similar-string-pairs
-  "주어진 문자열 리스트에서, 같은 위치에 정확히 하나의 문자가 다른 문자열 쌍을 찾아서 리턴한다"
-  [strings]
-  (for [i (range (count strings))
-        j (range (inc i) (count strings))
-        :let [source (nth strings i)
-              target (nth strings j)]
-        :when (= 1 (similarity-scoring-string-pair source target))]
-    [source target]))
 
-(defn find-common-part
+(def sample-input-part2 (str/split-lines "abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz"))
+
+
+(defn generate-word-pairs
+  "주어진 문자열 리스트에서, 같은 위치에 정확히 하나의 문자가 다른 문자열 쌍을 찾아서 리턴한다"
+  [words]
+  (for [i (range (count words))
+        j (range (inc i) (count words))
+        :let [source (nth words i)
+              target (nth words j)]]
+    {:source source
+     :target target}))
+
+(defn merge-same-position-char
   "두 문자열에서 같은 위치에 있는 문자가 같은 부분만을 리턴한다"
-  [[source target]]
-  (apply str
-         (for [[first-character second-character] (map vector (seq source) (seq target))
-               :when (= first-character second-character)] ; 문자가 같은 경우에만 포함
-           first-character)))
+  [word-pair]
+  (->> (map vector (:source word-pair) (:target word-pair))
+       (filter (fn [[source-char target-char]]
+                 (= source-char target-char)))
+       (map first)
+       (apply str)))
+
+(defn calculate-diff-char-count
+  "두 문자열의 다른 문자 개수를 반환한다."
+  [word-pair]
+  (->> (map vector (:source word-pair) (:target word-pair))
+       (filter (fn [[source-char target-char]]
+                 (not= source-char target-char)))
+       (count)))
+
+(defn solve-part2
+  [input]
+  (->> (generate-word-pairs input)
+       (filter (fn [word-pair]
+                 (= 1 (calculate-diff-char-count word-pair))))
+       (map merge-same-position-char)
+       (first)))
+
+(defn inventory-management-system-part2
+  [input]
+  (solve-part2 input))
 
 (comment
-  ;; map 으로 해도 무방함
-  ;; break 등 라벨이동 하는 코드는 지양해야한다.
-  (doseq [pair (find-similar-string-pairs (clojure.string/split-lines
-                                            (slurp "resources/aoc2018_2.sample.part2.txt")))]
-    (println (find-common-part pair))))
+  #_(inventory-management-system-part2 sample-input-part2)
+  (inventory-management-system-part2 (->> (slurp "resources/aoc2018_2.sample.txt")
+                                          (str/split-lines))))
 
 ;; 주어진 예시에서 fguij와 fghij는 같은 위치 (2번째 인덱스)에 정확히 한 문자 (u와 h)가 다름. 따라서 같은 부분인 fgij를 리턴하면 됨.
 
